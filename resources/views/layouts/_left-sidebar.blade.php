a5f476f2ff4a044bd89fbcbc6ef66116235d0f95
abdur-rahman <rjarif0958@gmail.com>
2023-04-05 21:07:19 +0600

    navbar added

--- resources/views/layouts/_left-sidebar.blade.php
+++ resources/views/layouts/_left-sidebar.blade.php
@@ -1,8 +1,14 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="#" class="brand-link">
+        <img src="/asset/dist/img/logo.png" alt="{{config('app.name')}}" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-bold">
+            {{ config('app.name')}}
+        </span>
+    </a>
     <!-- Sidebar -->
     <div class="sidebar">
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
